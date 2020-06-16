1b599ca081320314f51e1bbbc98a1966f14149ab
Osasumwen Usen <osas.usen@gmail.com>
2020-06-16 17:43:19 +0100

    Initial Commit

--- flask_server/app.py
+++ flask_server/app.py
@@ -50,5 +50,5 @@ def image_quality():
     return jsonify(data)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000, debug=True)
